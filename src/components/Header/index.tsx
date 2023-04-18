ba491fc6b2138942283f76660af94c69a8e6c5f9
Harlley <harlleybastos@hotmail.com>
2023-04-18 10:16:40 -0300

    styles: fix hamburguer menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "unset";
+    }
+  }, [isOpen]);
 
   return (
     <header className="lg:max-w-[1200px] lg:mx-auto p-3 flex justify-between items-center border-b border-b-gray-400">
@@ -15,18 +23,19 @@ const Header = () => {
           src="/img/logo-without-slogan.png"
         />
       </div>
-      <div className="lg:hidden relative">
+      <div className="lg:hidden relative z-10">
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="text-gray-500 hover:text-black focus:outline-none"
         >
           <svg
-            className="h-6 w-6 fill-current"
+            className="h-6 w-6 fill-current z-20"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             {isOpen ? (
               <path
+                className="z-10"
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M18 8H6V6h12v2zm-2 5H6v-2h10v2zm2 5H6v-2h12v2z"
@@ -44,7 +53,7 @@ const Header = () => {
       <ul
         className={`${
           isOpen
-            ? "flex absolute top-[9vh] right-0 left-0 bottom-0 flex-col justify-between p-20 items-center bg-white"
+            ? "flex absolute top-0 h-full right-0 left-0 bottom-0 flex-col justify-between p-20 items-center bg-white"
             : "hidden"
         } lg:flex lg:gap-5 mt-2 lg:mt-0 transition-all ease-in-out duration-300`}
       >
